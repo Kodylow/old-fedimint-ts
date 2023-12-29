3fe91e8b23278fb4c823674ad1b511092874b4cd
Conner Ow <connerow1115@gmail.com>
2023-12-29 10:55:55 -0600

    Add linting + Document + Improve Modularity

--- FedimintClient.ts
+++ FedimintClient.ts
@@ -1,129 +1,253 @@
-import { BackupRequest, InfoResponse, ListOperationsRequest, OperationOutput } from "./types/common";
-import { AwaitInvoiceRequest, AwaitLnPayRequest, Gateway, LnInvoiceRequest, LnInvoiceResponse, LnPayRequest, LnPayResponse, SwitchGatewayRequest } from "./types/modules/ln";
-import { CombineRequest, CombineResponse, ReissueRequest, ReissueResponse, SpendRequest, SpendResponse, SplitRequest, SplitResponse, ValidateRequest, ValidateResponse } from "./types/modules/mint";
-import { AwaitDepositRequest, AwaitDepositResponse, DepositAddressRequest, DepositAddressResponse, WithdrawRequest, WithdrawResponse } from "./types/modules/wallet";
+import type {
+  BackupRequest,
+  InfoResponse,
+  ListOperationsRequest,
+  OperationOutput,
+} from "./types/common";
+import type {
+  AwaitInvoiceRequest,
+  AwaitLnPayRequest,
+  Gateway,
+  LnInvoiceRequest,
+  LnInvoiceResponse,
+  LnPayRequest,
+  LnPayResponse,
+  SwitchGatewayRequest,
+} from "./types/modules/ln";
+import type {
+  CombineRequest,
+  CombineResponse,
+  ReissueRequest,
+  ReissueResponse,
+  SpendRequest,
+  SpendResponse,
+  SplitRequest,
+  SplitResponse,
+  ValidateRequest,
+  ValidateResponse,
+} from "./types/modules/mint";
+import type {
+  AwaitDepositRequest,
+  AwaitDepositResponse,
+  DepositAddressRequest,
+  DepositAddressResponse,
+  WithdrawRequest,
+  WithdrawResponse,
+} from "./types/modules/wallet";
 
 type FedimintResponse<T> = Promise<T>;
 
-type FedimintClientParams = {
-    baseUrl: string;
-    password: string;
-}
-
 class FedimintClient {
-    private baseUrl: string;
-    private password: string;
+  private baseUrl: string;
+  private password: string;
 
-    constructor({ baseUrl, password }: FedimintClientParams) {
-        this.baseUrl = baseUrl + '/fedimint/v2'
-        this.password = password;
+  constructor({ baseUrl, password }: { baseUrl: string; password: string }) {
+    this.baseUrl = baseUrl + "/fedimint/v2";
+    this.password = password;
+  }
+
+  /**
+   * Makes a GET request to the `baseURL` at the given `endpoint`.
+   * Receives a JSON response.
+   * @param endpoint - The endpoint to make the request to.
+   */
+  private async get<T>(endpoint: string): FedimintResponse<T> {
+    const res = await fetch(`${this.baseUrl}${endpoint}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.password}` },
+    });
+
+    if (!res.ok) {
+      throw new Error(`GET request failed with status ${res.status}`);
     }
 
-    private async fetchWithAuth<T>(endpoint: string, options: RequestInit): FedimintResponse<T> {
-        const response = await fetch(`${this.baseUrl}${endpoint}`, options);
+    return (await res.json()) as T;
+  }
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+  /**
+   * Makes a POST request to the `baseURL` at the given `endpoint` with the provided `body`.
+   * Receives a JSON response.
+   * @param endpoint - The endpoint to make the request to.
+   * @param body - The body of the request.
+   */
+  private async post<T>(endpoint: string, body: any): FedimintResponse<T> {
+    const res = await fetch(`${this.baseUrl}${endpoint}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.password}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-        return await response.json() as T;
+    if (!res.ok) {
+      throw new Error(`POST request failed with status ${res.status}`);
     }
 
-    private async get<T>(endpoint: string): FedimintResponse<T> {
-        return this.fetchWithAuth<T>(endpoint, { method: 'GET', headers: { 'Authorization': `Bearer ${this.password}` } });
-    }
+    return (await res.json()) as T;
+  }
 
-    private async post<T>(endpoint: string, body: any): FedimintResponse<T> {
-        return this.fetchWithAuth<T>(endpoint, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${this.password}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body)
-        });
-    }
+  /**
+   * Fetches wallet information including holdings, tiers, and federation metadata.
+   */
+  public async info(): FedimintResponse<InfoResponse> {
+    return await this.get<InfoResponse>("/admin/info");
+  }
 
-    public async info(): FedimintResponse<InfoResponse> {
-        return this.get<InfoResponse>('/admin/info');
-    }
+  /**
+   * Uploads the encrypted snapshot of mint notest to the federation
+   */
+  public async backup(metadata: BackupRequest): FedimintResponse<void> {
+    await this.post<void>("/admin/backup", metadata);
+  }
 
-    public async backup(metadata: BackupRequest): FedimintResponse<void> {
-        return this.post<void>('/admin/backup', metadata);
-    }
+  /**
+   * Returns the API version to use to communicate with the federation
+   */
+  public async discoverVersion(): FedimintResponse<string> {
+    return this.get<string>("/admin/discover-version");
+  }
 
-    public async discoverVersion(): FedimintResponse<string> {
-        return this.get<string>('/admin/discover-version');
-    }
+  /**
+   * Outputs a list of operations that have been performed on the federation
+   */
+  public async listOperations(
+    request: ListOperationsRequest,
+  ): FedimintResponse<OperationOutput[]> {
+    return await this.post<OperationOutput[]>(
+      "/admin/list-operations",
+      request,
+    );
+  }
 
-    // NOTE: unimplemented
-    // public async restore(): FedimintResponse<void> {
-    //     return this.post<void>('/admin/restore', {});
-    // }
+  /**
+   * Returns the client configuration for the federation
+   */
+  public async config(): FedimintResponse<any> {
+    return await this.get<any>("/admin/config");
+  }
 
-    public async listOperations(request: ListOperationsRequest): FedimintResponse<OperationOutput[]> {
-        return this.post<OperationOutput[]>('/admin/list-operations', request);
-    }
+  /**
+   * A Module for interacting with Lightning
+   */
+  public ln = {
+    /**
+     * Creates a lightning invoice to receive payment via gateway
+     */
+    createInvoice: async (
+      request: LnInvoiceRequest,
+    ): FedimintResponse<LnInvoiceResponse> =>
+      await this.post<LnInvoiceResponse>("/ln/invoice", request),
 
-    // NOTE: unimplemented
-    // public async module(request: any): FedimintResponse<any> {
-    //     return this.post<any>('/admin/module', request);
-    // }
+    /**
+     * Waits for a lightning invoice to be paid
+     */
+    awaitInvoice: async (
+      request: AwaitInvoiceRequest,
+    ): FedimintResponse<InfoResponse> =>
+      await this.post<InfoResponse>("/ln/await-invoice", request),
 
-    public async config(): FedimintResponse<any> {
-        return this.get<any>('/admin/config');
-    }
+    /**
+     * Pays a lightning invoice or lnurl via a gateway
+     */
+    pay: async (request: LnPayRequest): FedimintResponse<LnPayResponse> =>
+      await this.post<LnPayResponse>("/ln/pay", request),
 
-    public modules = {
-        ln: {
-            createInvoice: async (request: LnInvoiceRequest): FedimintResponse<LnInvoiceResponse> => {
-                return this.post<LnInvoiceResponse>('/ln/invoice', request);
-            },
-            awaitInvoice: async (request: AwaitInvoiceRequest): FedimintResponse<InfoResponse> => {
-                return this.post<InfoResponse>('/ln/await-invoice', request);
-            },
-            pay: async (request: LnPayRequest): FedimintResponse<LnPayResponse> => {
-                return this.post<LnPayResponse>('/ln/pay', request);
-            },
-            awaitPay: async (request: AwaitLnPayRequest): FedimintResponse<LnPayResponse> => {
-                return this.post<LnPayResponse>('/ln/await-pay', request);
-            },
-            listGateways: async (): FedimintResponse<Gateway[]> => {
-                return this.get<Gateway[]>('/ln/list-gateways');
-            },
-            switchGateway: async (request: SwitchGatewayRequest): FedimintResponse<Gateway> => {
-                return this.post<Gateway>('/ln/switch-gateway', request);
-            },
-        },
-        wallet: {
-            createDepositAddress: async (request: DepositAddressRequest): FedimintResponse<DepositAddressResponse> => {
-                return this.post<DepositAddressResponse>('/wallet/deposit-address', request);
-            },
-            awaitDeposit: async (request: AwaitDepositRequest): FedimintResponse<AwaitDepositResponse> => {
-                return this.post<AwaitDepositResponse>('/wallet/await-deposit', request);
-            },
-            withdraw: async (request: WithdrawRequest): FedimintResponse<WithdrawResponse> => {
-                return this.post<WithdrawResponse>('/wallet/withdraw', request);
-            },
-        },
-        mint: {
-            reissue: async (request: ReissueRequest): FedimintResponse<ReissueResponse> => {
-                return this.post<ReissueResponse>('/mint/reissue', request);
-            },
-            spend: async (request: SpendRequest): FedimintResponse<SpendResponse> => {
-                return this.post<SpendResponse>('/mint/spend', request);
-            },
-            validate: async (request: ValidateRequest): FedimintResponse<ValidateResponse> => {
-                return this.post<ValidateResponse>('/mint/validate', request);
-            },
-            split: async (request: SplitRequest): FedimintResponse<SplitResponse> => {
-                return this.post<SplitResponse>('/mint/split', request);
-            },
-            combine: async (request: CombineRequest): FedimintResponse<CombineResponse> => {
-                return this.post<CombineResponse>('/mint/combine', request);
-            },
-        },
-    }
+    /**
+     * Waits for a lightning payment to complete
+     */
+    awaitPay: async (
+      request: AwaitLnPayRequest,
+    ): FedimintResponse<LnPayResponse> =>
+      await this.post<LnPayResponse>("/ln/await-pay", request),
+
+    /**
+     * Outputs a list of registered lighting lightning gateways
+     */
+    listGateways: async (): FedimintResponse<Gateway[]> =>
+      await this.get<Gateway[]>("/ln/list-gateways"),
+
+    /**
+     * Switches the active lightning gateway
+     */
+    switchGateway: async (
+      request: SwitchGatewayRequest,
+    ): FedimintResponse<Gateway> =>
+      await this.post<Gateway>("/ln/switch-gateway", request),
+  };
+
+  /**
+   * A module for creating a bitcoin deposit address
+   */
+  public wallet = {
+    /**
+     * Creates a new bitcoin deposit address
+     */
+    createDepositAddress: async (
+      request: DepositAddressRequest,
+    ): FedimintResponse<DepositAddressResponse> =>
+      await this.post<DepositAddressResponse>(
+        "/wallet/deposit-address",
+        request,
+      ),
+
+    /**
+     * Waits for a bitcoin deposit to be confirmed
+     */
+    awaitDeposit: async (
+      request: AwaitDepositRequest,
+    ): FedimintResponse<AwaitDepositResponse> =>
+      await this.post<AwaitDepositResponse>("/wallet/await-deposit", request),
+
+    /**
+     * Withdraws bitcoin from the federation
+     */
+    withdraw: async (
+      request: WithdrawRequest,
+    ): FedimintResponse<WithdrawResponse> =>
+      await this.post<WithdrawResponse>("/wallet/withdraw", request),
+  };
+
+  /**
+   * A module for interacting with an ecash mint
+   */
+  public mint = {
+    /**
+     * Reissues an ecash note
+     */
+    reissue: async (
+      request: ReissueRequest,
+    ): FedimintResponse<ReissueResponse> =>
+      await this.post<ReissueResponse>("/mint/reissue", request),
+
+    /**
+     * Spends an ecash note
+     */
+    spend: async (request: SpendRequest): FedimintResponse<SpendResponse> =>
+      await this.post<SpendResponse>("/mint/spend", request),
+
+    /**
+     * Validates an ecash note
+     */
+    validate: async (
+      request: ValidateRequest,
+    ): FedimintResponse<ValidateResponse> =>
+      await this.post<ValidateResponse>("/mint/validate", request),
+
+    /**
+     * Splits an ecash note
+     */
+    split: async (request: SplitRequest): FedimintResponse<SplitResponse> =>
+      await this.post<SplitResponse>("/mint/split", request),
+
+    /**
+     * Combines ecash notes
+     */
+    combine: async (
+      request: CombineRequest,
+    ): FedimintResponse<CombineResponse> =>
+      await this.post<CombineResponse>("/mint/combine", request),
+  };
 }
 
 export default FedimintClient;
